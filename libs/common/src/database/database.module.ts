d947db3e9936239074aa015f134b27bada13233f
pangeran.jonathan <joo@DESKTOP-KAGCEJU>
2023-07-13 00:38:36 +0700

    feat: add config module and database module to use accross the microservices

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      })
+    }),
+  ]
+})
+export class DatabaseModule {}
